d960c1338bb63b687a8b84a2f7f026882436545c
Murad501 <mm845635@gmail.com>
2022-11-08 01:33:23 +0600

    home page services api created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+require("dotenv").config();
 const cors = require('cors')
 const port = process.env.PORT || 5000
 
@@ -16,6 +17,21 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@user1.istzhai.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const run = async() => {
+    try{
+        const servicesCollection = client.db('Rohimas-Kitchen').collection('services');
+
+        app.get('/home', async(req, res) => {
+            const query = {}
+            const cursor = servicesCollection.find(query)
+            const result = await cursor.limit(3).toArray()
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.error(err))
 
 
 app.listen(port, ()=> {
