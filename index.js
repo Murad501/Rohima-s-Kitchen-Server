405e370f46e32b9b950a17f3f223f9f1820503db
Murad501 <mm845635@gmail.com>
2022-11-08 01:23:55 +0600

    mongodb basic setup done

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ app.get('/', (req, res) => {
     res.send('data is loading')
 })
 
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@user1.istzhai.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
 app.listen(port, ()=> {
     console.log('server running on port', port)
 })
